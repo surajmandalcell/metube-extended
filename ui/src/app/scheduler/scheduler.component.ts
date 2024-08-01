1fb8209bdb3b621707ca1c996b35cb10e1e9d616
Suraj Mandal <surajmandalcell@gmail.com>
2024-08-01 19:12:28 -0400

    SNAPSHOT: things seem to be looking the way I wantthem to

--- ui/src/app/scheduler/scheduler.component.ts
+++ ui/src/app/scheduler/scheduler.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { CookieService } from 'ngx-cookie-service';
+import { Formats, Format, Quality } from './../formats';
+import { DownloadsService } from './../downloads.service';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 interface Schedule {
   id: number;
@@ -17,7 +21,7 @@ interface Schedule {
   templateUrl: './scheduler.component.html',
   styleUrls: ['./scheduler.component.sass'],
 })
-export class SchedulerComponent implements OnInit {
+export class SchedulerComponent implements OnInit, OnDestroy {
   schedules: Schedule[] = [];
   newSchedule: Schedule = {
     id: 0,
@@ -28,82 +32,197 @@ export class SchedulerComponent implements OnInit {
     next_run: null,
   };
   editingSchedule: Schedule | null = null;
+  formats: Format[] = Formats;
+  qualities: Quality[];
+  quality: string;
+  format: string;
+  folder: string;
+  customDirs$: Observable<string[]>;
+  cronIntervals = [
+    { value: '0 * * * *', label: 'Every hour' },
+    { value: '0 */2 * * *', label: 'Every 2 hours' },
+    { value: '0 */4 * * *', label: 'Every 4 hours' },
+    { value: '0 */6 * * *', label: 'Every 6 hours' },
+    { value: '0 */8 * * *', label: 'Every 8 hours' },
+    { value: '0 */10 * * *', label: 'Every 10 hours' },
+    { value: '0 */12 * * *', label: 'Every 12 hours' },
+    { value: '0 0 * * *', label: 'Every 24 hours' },
+    { value: 'custom', label: 'Custom' },
+  ];
+  selectedInterval = '0 * * * *';
+  customCron = '';
 
   faTrashAlt = faTrashAlt;
   faEdit = faEdit;
   faPlus = faPlus;
 
-  constructor(private http: HttpClient) {}
+  private subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+    public downloads: DownloadsService
+  ) {
+    this.format = cookieService.get('metube_format') || 'any';
+    this.setQualities();
+    this.quality = cookieService.get('metube_quality') || 'best';
+  }
 
   ngOnInit() {
     this.loadSchedules();
+    this.customDirs$ = this.getMatchingCustomDir();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   loadSchedules() {
-    this.http.get<Schedule[]>('scheduler/list').subscribe(
-      (data) => {
-        this.schedules = data;
-      },
-      (error) => {
-        console.error('Error fetching schedules:', error);
-      }
+    this.subscriptions.add(
+      this.http.get<Schedule[]>('scheduler/list').subscribe({
+        next: (data) => {
+          this.schedules = data;
+        },
+        error: (error) => {
+          console.error('Error fetching schedules:', error);
+        },
+      })
     );
   }
 
   addSchedule() {
-    this.http.post<Schedule>('scheduler/add', this.newSchedule).subscribe(
-      (data) => {
-        this.schedules.push(data);
-        this.newSchedule = {
-          id: 0,
-          url: '',
-          cron: '',
-          folder: '',
-          last_run: null,
-          next_run: null,
-        };
-      },
-      (error) => {
-        console.error('Error adding schedule:', error);
-      }
+    const cron =
+      this.selectedInterval === 'custom'
+        ? this.customCron
+        : this.selectedInterval;
+    this.subscriptions.add(
+      this.http
+        .post<Schedule>('scheduler/add', { ...this.newSchedule, cron })
+        .subscribe({
+          next: (data) => {
+            this.schedules.push(data);
+            this.resetNewSchedule();
+          },
+          error: (error) => {
+            console.error('Error adding schedule:', error);
+          },
+        })
     );
+  }
+
+  resetNewSchedule() {
+    this.newSchedule = {
+      id: 0,
+      url: '',
+      cron: '',
+      folder: '',
+      last_run: null,
+      next_run: null,
+    };
+    this.selectedInterval = '0 * * * *';
+    this.customCron = '';
   }
 
   updateSchedule() {
     if (this.editingSchedule) {
-      this.http
-        .post('scheduler/update', {
-          ids: [this.editingSchedule.id],
-          cron: this.editingSchedule.cron,
-        })
-        .subscribe(
-          () => {
-            this.loadSchedules();
-            this.editingSchedule = null;
-          },
-          (error) => {
-            console.error('Error updating schedule:', error);
-          }
-        );
+      const cron =
+        this.selectedInterval === 'custom'
+          ? this.customCron
+          : this.selectedInterval;
+      this.subscriptions.add(
+        this.http
+          .post('scheduler/update', {
+            ids: [this.editingSchedule.id],
+            cron: cron,
+          })
+          .subscribe({
+            next: () => {
+              this.loadSchedules();
+              this.editingSchedule = null;
+            },
+            error: (error) => {
+              console.error('Error updating schedule:', error);
+            },
+          })
+      );
     }
   }
 
   removeSchedule(id: number) {
-    this.http.post('scheduler/remove', { ids: [id] }).subscribe(
-      () => {
-        this.schedules = this.schedules.filter((s) => s.id !== id);
-      },
-      (error) => {
-        console.error('Error removing schedule:', error);
-      }
+    this.subscriptions.add(
+      this.http.post('scheduler/remove', { ids: [id] }).subscribe({
+        next: () => {
+          this.schedules = this.schedules.filter((s) => s.id !== id);
+        },
+        error: (error) => {
+          console.error('Error removing schedule:', error);
+        },
+      })
     );
   }
 
   startEdit(schedule: Schedule) {
     this.editingSchedule = { ...schedule };
+    const cronIndex = this.cronIntervals.findIndex(
+      (interval) => interval.value === schedule.cron
+    );
+    if (cronIndex !== -1) {
+      this.selectedInterval = schedule.cron;
+    } else {
+      this.selectedInterval = 'custom';
+      this.customCron = schedule.cron;
+    }
   }
 
   cancelEdit() {
     this.editingSchedule = null;
+    this.selectedInterval = '0 * * * *';
+    this.customCron = '';
+  }
+
+  setQualities() {
+    this.qualities = this.formats.find((el) => el.id == this.format).qualities;
+    const exists = this.qualities.find((el) => el.id === this.quality);
+    this.quality = exists ? this.quality : 'best';
+  }
+
+  formatChanged() {
+    this.cookieService.set('metube_format', this.format, { expires: 3650 });
+    this.setQualities();
+    this.downloads.customDirsChanged.next(this.downloads.customDirs);
+  }
+
+  qualityChanged() {
+    this.cookieService.set('metube_quality', this.quality, { expires: 3650 });
+    this.downloads.customDirsChanged.next(this.downloads.customDirs);
+  }
+
+  getMatchingCustomDir(): Observable<string[]> {
+    return this.downloads.customDirsChanged.asObservable().pipe(
+      map((output) => {
+        if (this.isAudioType()) {
+          console.debug('Showing audio-specific download directories');
+          return output['audio_download_dir'];
+        } else {
+          console.debug('Showing default download directories');
+          return output['download_dir'];
+        }
+      })
+    );
+  }
+
+  isAudioType() {
+    return (
+      this.quality == 'audio' ||
+      this.format == 'mp3' ||
+      this.format == 'm4a' ||
+      this.format == 'opus' ||
+      this.format == 'wav' ||
+      this.format == 'flac'
+    );
+  }
+
+  showAdvanced() {
+    return this.downloads.configuration['CUSTOM_DIRS'];
   }
 }
